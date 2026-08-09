57d4906ff3f3fa47f9d601ce33590b86b970839d
Charly JOUCLARD <jouclardcharly@gmail.com>
2026-08-09 22:40:52 +0200

    change pie chart to tree map

--- export_potential/display_data.py
+++ export_potential/display_data.py
@@ -28,48 +28,83 @@ def format_value(df) -> pd.DataFrame:
 
     return df
 
-def data_for_piechart(df) -> pd.DataFrame:
+def data_for_treemap(df) -> pd.DataFrame:
     value_world = df["primaryValue"].sum()
 
     df_sorted = df.dropna(subset=["primaryValue"]).sort_values(
         by="primaryValue", ascending=False
     )
 
-    top10 = df_sorted.head(20).copy()
+    top20 = df_sorted.head(20).copy()
 
-    # Calculer la somme du reste du monde
-    value_top10 = top10["primaryValue"].sum()
-    value_other = value_world - value_top10
+    value_top20 = top20["primaryValue"].sum()
+    value_other = value_world - value_top20
 
-    # Créer un DataFrame complet pour le Pie Chart
-    pie_df = pd.concat(
+    treemap_df = pd.concat(
         [
-            top10[["reporterCodeIsoAlpha3", "countryName", "primaryValue"]],
+            top20[["reporterCodeIsoAlpha3", "countryName", "primaryValue"]],
             pd.DataFrame(
                 [{"reporterCodeIsoAlpha3": "Other", "countryName": "Other", "primaryValue": value_other}]
             ),
         ],
         ignore_index=True,
     )
-    return pie_df
+    return treemap_df
+
+def plot_map(df, fig):
+    fig.add_trace(
+        go.Choropleth(
+            locations=df["reporterCodeIsoAlpha3"],
+            z=df["RSCA"],
+            colorscale="Haline",
+            colorbar_title="RSCA",
+            colorbar_x=-0.13,
+            customdata=df[["text", "qty", "primaryValue_str", "RSCA"]],
+            hovertemplate="<b>%{hovertext}</b><br><br>"
+            + "Product : %{customdata[0]}<br>"
+            + "Quantity : %{customdata[1]}<br>"
+            + "Value (USD) : %{customdata[2]}<br>"
+            + "RSCA : %{customdata[3]:.2f}<extra></extra>",
+            hovertext=df["countryName"],
+        ),
+        row=1,
+        col=1,
+    )
+
+def plot_treemap(df, fig):
+    fig.add_trace(
+        go.Treemap(
+            labels=df["countryName"],
+            parents=[""] * len(df),
+            values=df["primaryValue"],
+            customdata=df[["primaryValue_str"]],
+            texttemplate="<b>%{label}</b><br>%{percentEntry:.1%}",
+            hovertemplate="<b>%{label}</b><br>"
+            + "Total exportation : %{customdata[0]}<br>"
+            + "Global share : %{percentEntry:.2%}<extra></extra>",
+        ),
+        row=1,
+        col=2,
+    )
+
 
 
 data = get_data()
 data = exclude_total_export(data)
 
 choloro_df = data.copy()
-pie_df = data.copy()
+treemap_df = data.copy()
 
 choloro_df = format_value(choloro_df)
 choloro_df = format_qty(choloro_df)
 
-pie_df = data_for_piechart(pie_df)
-pie_df = format_value(pie_df)
+treemap_df = data_for_treemap(treemap_df)
+treemap_df = format_value(treemap_df)
 
 fig = make_subplots(
     rows=1,
     cols=2,
-    column_widths=[0.6, 0.4],
+    column_widths=[0.7, 0.3],
     specs=[[{"type": "choropleth"}, {"type": "pie"}]],
     subplot_titles=(
         "Indice RSCA par pays",
@@ -77,41 +112,8 @@ fig = make_subplots(
     ),
 )
 
-# 4. Carte Choroplèthe (Gauche)
-fig.add_trace(
-    go.Choropleth(
-        locations=choloro_df["reporterCodeIsoAlpha3"],
-        z=choloro_df["RSCA"],
-        colorscale="Haline",
-        colorbar_title="RSCA",
-        colorbar_x=0.53,
-        customdata=choloro_df[["text", "qty", "primaryValue_str", "RSCA"]],
-        hovertemplate="<b>%{hovertext}</b><br><br>"
-        + "Product : %{customdata[0]}<br>"
-        + "Quantity : %{customdata[1]}<br>"
-        + "Value (USD) : %{customdata[2]}<br>"
-        + "RSCA : %{customdata[3]:.2f}<extra></extra>",
-        hovertext=choloro_df["countryName"],
-    ),
-    row=1,
-    col=1,
-)
-
-# 5. Pie / Donut Chart exact (Droite)
-fig.add_trace(
-    go.Treemap(
-        labels=pie_df["countryName"],
-        parents=[""] * len(pie_df),
-        values=pie_df["primaryValue"],
-        customdata=pie_df[["primaryValue_str"]],
-        texttemplate="<b>%{label}</b><br>%{percentEntry:.1%}",
-        hovertemplate="<b>%{label}</b><br>"
-        + "Total exportation : %{customdata[0]}<br>"
-        + "Global share : %{percentEntry:.2%}<extra></extra>",
-    ),
-    row=1,
-    col=2,
-)
+plot_map(choloro_df,fig)
+plot_treemap(treemap_df,fig)
 
 # 6. Configuration de la mise en page
 fig.update_layout(
