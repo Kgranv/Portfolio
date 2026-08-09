ab4789d73598404261908c6ceb8fc4c09c2dd167
Charly JOUCLARD <jouclardcharly@gmail.com>
2026-08-09 21:39:22 +0200

    add country name

--- export_potential/prepare_data.py
+++ export_potential/prepare_data.py
@@ -12,7 +12,8 @@ def filter_data(raw_data) -> pd.DataFrame:
 
 def add_iso_code(df) -> pd.DataFrame:
     reporter_data = pd.read_parquet("./data/reporter.parquet")
-    df = df.merge(reporter_data[["reporterCode", "reporterCodeIsoAlpha3"]], on="reporterCode", how="inner")
+    df = df.merge(reporter_data[["reporterCode", "reporterCodeIsoAlpha3", "text"]], on="reporterCode", how="inner")
+    df.rename(columns={'text': 'countryName'}, inplace=True)
     return df
 
 def add_goods_description(df) -> pd.DataFrame:
