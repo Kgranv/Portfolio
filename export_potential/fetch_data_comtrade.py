ebf89bf24c3aee61d7669c7c78dd4fa80b310359
Charly JOUCLARD <jouclardcharly@gmail.com>
2026-08-10 02:43:32 +0200

    remove group of country in example

--- export_potential/fetch_data_comtrade.py
+++ export_potential/fetch_data_comtrade.py
@@ -73,27 +73,27 @@ def main():
         # For oils
         # cmdCode_Dict = {"270900":"OILS"} 
         # reporterCode_Dict = {682: 'SAU', 784: 'ARE', 124: 'CAN', 842: 'USA', 368: 'IRQ', 414: 'KWT', 579: 'NOR', 566: 'NGA', 398: 'KAZ', 76: 'BRA', 24: 'AGO', 484: 'MEX', 512: 'OMN', 826: 'GBR', 364: 'IRN', 328: 'GUY', 31: 'AZE', 634: 'QAT', 170: 'COL', 218: 'ECU'}
-        
+
         # For copper
         # cmdCode_Dict = {"260300":"COPPER"} 
-        # reporterCode_Dict = {152: 'CHL', 604: 'PER', 975: 'R4 ', 360: 'IDN', 36: 'AUS', 124: 'CAN', 484: 'MEX', 842: 'USA', 591: 'PAN', 76: 'BRA', 496: 'MNG', 398: 'KAZ', 688: 'SRB', 97: 'EUR', 490: 'S19', 410: 'KOR', 218: 'ECU', 724: 'ESP', 268: 'GEO', 598: 'PNG'}
+        # reporterCode_Dict = {152: 'CHL', 604: 'PER', 360: 'IDN', 36: 'AUS', 124: 'CAN', 484: 'MEX', 842: 'USA', 591: 'PAN', 76: 'BRA', 496: 'MNG', 398: 'KAZ', 688: 'SRB', 410: 'KOR', 218: 'ECU', 724: 'ESP', 268: 'GEO', 598: 'PNG', 682: 'SAU', 51: 'ARM', 458: 'MYS'}
         
         # For Lithium
         # cmdCode_Dict = {"282520":"LITHIUM"} 
-        # reporterCode_Dict = {156: 'CHN', 152: 'CHL', 842: 'USA', 528: 'NLD', 410: 'KOR', 826: 'GBR', 97: 'EUR', 56: 'BEL', 251: 'FRA', 784: 'ARE', 392: 'JPN', 699: 'IND', 233: 'EST', 616: 'POL', 642: 'ROU', 757: 'CHE', 124: 'CAN', 566: 'NGA', 792: 'TUR', 705: 'SVN'}
-        
+        # reporterCode_Dict = {156: 'CHN', 152: 'CHL', 842: 'USA', 528: 'NLD', 410: 'KOR', 826: 'GBR', 56: 'BEL', 251: 'FRA', 784: 'ARE', 392: 'JPN', 699: 'IND', 233: 'EST', 616: 'POL', 642: 'ROU', 757: 'CHE', 124: 'CAN', 566: 'NGA', 792: 'TUR', 705: 'SVN', 203: 'CZE'}
+            
         # For wood
         # cmdCode_Dict = {"4403":"WOOD"} 
-        # reporterCode_Dict = {554: 'NZL', 97: 'EUR', 842: 'USA', 276: 'DEU', 203: 'CZE', 858: 'URY', 124: 'CAN', 251: 'FRA', 616: 'POL', 428: 'LVA', 528: 'NLD', 56: 'BEL', 579: 'NOR', 598: 'PNG', 752: 'SWE', 76: 'BRA', 178: 'COG', 703: 'SVK', 724: 'ESP', 705: 'SVN'}
-        
+        # reporterCode_Dict = {554: 'NZL', 842: 'USA', 276: 'DEU', 203: 'CZE', 858: 'URY', 124: 'CAN', 251: 'FRA', 616: 'POL', 428: 'LVA', 528: 'NLD', 56: 'BEL', 579: 'NOR', 598: 'PNG', 752: 'SWE', 76: 'BRA', 178: 'COG', 703: 'SVK', 724: 'ESP', 705: 'SVN', 233: 'EST'}
+         
         # For Cocoa
         # cmdCode_Dict = {"180100":"COCOA"} 
-        # reporterCode_Dict = {384: 'CIV', 288: 'GHA', 218: 'ECU', 566: 'NGA', 120: 'CMR', 528: 'NLD', 458: 'MYS', 604: 'PER', 180: 'COD', 598: 'PNG', 800: 'UGA', 97: 'EUR', 360: 'IDN', 56: 'BEL', 233: 'EST', 450: 'MDG', 834: 'TZA', 276: 'DEU', 768: 'TGO', 170: 'COL'}
-        
+        # reporterCode_Dict = {384: 'CIV', 288: 'GHA', 218: 'ECU', 566: 'NGA', 120: 'CMR', 528: 'NLD', 458: 'MYS', 604: 'PER', 180: 'COD', 598: 'PNG', 800: 'UGA', 360: 'IDN', 56: 'BEL', 233: 'EST', 450: 'MDG', 834: 'TZA', 276: 'DEU', 768: 'TGO', 170: 'COL', 842: 'USA'}
+
         # For wheat
         cmdCode_Dict = {"110100":"WHEAT"} 
-        reporterCode_Dict = {792: 'TUR', 398: 'KAZ', 276: 'DEU', 97: 'EUR', 699: 'IND', 860: 'UZB', 380: 'ITA', 32: 'ARG', 124: 'CAN', 842: 'USA', 56: 'BEL', 818: 'EGY', 826: 'GBR', 704: 'VNM', 251: 'FRA', 512: 'OMN', 392: 'JPN', 528: 'NLD', 348: 'HUN', 784: 'ARE'}
-
+        reporterCode_Dict = {792: 'TUR', 398: 'KAZ', 276: 'DEU', 699: 'IND', 860: 'UZB', 380: 'ITA', 32: 'ARG', 124: 'CAN', 842: 'USA', 56: 'BEL', 818: 'EGY', 826: 'GBR', 704: 'VNM', 251: 'FRA', 512: 'OMN', 392: 'JPN', 528: 'NLD', 348: 'HUN', 784: 'ARE', 156: 'CHN'}
+        
         year = "2022"
 
         for cmdCode,goods in cmdCode_Dict.items():
